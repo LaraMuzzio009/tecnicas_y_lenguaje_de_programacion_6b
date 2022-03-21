3e97d2db52be91b633921209091c94d0e0fff0ee
LaraMuzzio009 <101115588+LaraMuzzio009@users.noreply.github.com>
2022-03-21 10:18:28 -0300

    Add files via upload

---
+++ alumno_3.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-03-2022 a las 14:15:47
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `linaza_muzzio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id_legajo` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `domicilio` varchar(50) NOT NULL,
+  `teléfono` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id_legajo`, `nombre`, `apellido`, `domicilio`, `teléfono`) VALUES
+(1, 'Camila', 'Linaza', 'Bv.Illia 413', 152883098),
+(2, 'Lara', 'Muzzio', 'Independencia 209', 153549195),
+(3, 'Lucia', 'Bernasconi', 'Jose Hening 769', 155189687),
+(4, 'Celeste', 'Medràn', 'Bv San Juan 212', 153508149),
+(5, 'Martina', 'Muzzio', 'Independencia 209', 155290589),
+(6, 'Azul', 'Martinetto', 'Suecia 749', 152303238),
+(7, 'Julieta', 'Rassi', 'Ituazaingo 302', 156504868),
+(8, 'Agostina', 'Ricca', 'Lagunilla 1780', 153392183),
+(9, 'Ayra', 'Yiru', 'Brasil 120', 153080204),
+(10, 'Francisco', 'Re', 'San Lorenzo 420', 157366651),
+(11, 'Veronica', 'Diaz', 'San Martín 300', 155383578),
+(12, 'Agustin', 'Torres', 'Bv Chacabuco 350', 156358597),
+(13, 'Laura', 'Ribulgo', 'Jose Hening 780', 155334272),
+(14, 'Agustin', 'Negro', 'Dean Funes 454', 157141559),
+(15, 'Joaquin', 'Molina', 'Corrientes 600', 157422712);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id_legajo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id_legajo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ empresa_3.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-03-2022 a las 14:16:31
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empresa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `DNI` int(11) NOT NULL,
+  `teléfono` int(11) DEFAULT NULL,
+  `Domicilio` varchar(50) NOT NULL,
+  `Edad` int(11) DEFAULT NULL,
+  `Fecha_nac` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `apellido`, `DNI`, `teléfono`, `Domicilio`, `Edad`, `Fecha_nac`) VALUES
+(2, 'Mariana', 'Lopez', 29347122, 4850078, 'Santa Rosa 560', 28, '1997-04-14'),
+(3, 'Camila', 'Linaza', 46372933, 152883098, 'Bv.Iliia 413', 16, '2005-04-08'),
+(4, 'Lara', 'Muzzio', 46309949, 153549195, 'Independencia 209', 17, '2005-02-19'),
+(5, 'Veronica', 'Olmos', 30233245, 156079843, 'Aconquija 2455', 38, '1983-07-16'),
+(6, 'Norma', 'Aguirre', 20563409, 156454137, 'Independencia 209', 50, '1972-04-02'),
+(7, 'Javier', 'Linaza', 28244332, 156018847, 'Bv.Illia 413', 41, '1980-09-21'),
+(8, 'Alejandro', 'Muzzio', 20456921, 155946962, 'Independencia 209', 62, '1959-12-04'),
+(9, 'Juliana', 'Montenegro', 59954633, 15362075, 'Aconquija 2455', 2, '2019-07-10'),
+(10, 'Bibiana ', 'Aguirre', 20458791, 155733060, 'La Pampa 456', 62, '1959-12-03'),
+(11, 'Gastón', 'Olmos', 35697802, 152825691, 'Roma 1273', 33, '1988-09-08'),
+(12, 'Lourdes', 'Muzzio', 45304678, 152803766, 'Rio Negro 588', 19, '2003-12-15'),
+(13, 'Chiara', 'Muzzio', 45254171, 152456251, 'Independencia 209', 21, '2000-03-27'),
+(14, 'Elvira', 'Weinbender', 20358471, 155467150, 'La Pampa 1213', 66, '1960-04-21'),
+(15, 'gabriel', 'Muzzio', 18512435, 156856359, 'Independencia 209', 63, '1959-09-30'),
+(16, 'Abel', 'Olmos', 15123506, 156266904, 'La Pampa 1213', 67, '1955-05-27'),
+(17, 'Eugenia', 'Battaglino', 25385364, 156534861, 'Av. Richierri 2563', 50, '1972-08-20');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
